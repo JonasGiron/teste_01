2bd2ced210e224658f1a09388dd2dd894cb970f5
JonasGiron <jonasgiron@hotmail.com>
2023-07-10 21:36:42 -0300

    criado funcoes de testes

--- teste.js
+++ teste.js
@@ -3,5 +3,14 @@ function aplicardesconto(valor,desconto)
     return valor - desconto
 }
 
+function validanumero(valor) 
+{
+    return !isNaN(valor)
+}
+
 console.log('APP funcionando')
-console.log(aplicardescontotest())
+console.log('aplicardescontotest()')
+
+module.exports ={aplicardesconto}
+module.exports ={validanumero}
+
